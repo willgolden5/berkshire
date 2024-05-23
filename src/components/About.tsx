e2d0f875af6fb0e0bfb9b79ab02e2f003b921a11
William Golden <wtgolden5@gmail.com>
2024-05-23 09:45:44 -0400

    remove unneeded import

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect } from "react";
+import React, { forwardRef } from "react";
 import Slider from "react-slick";
 import { useStaticQuery, graphql } from "gatsby";
 
